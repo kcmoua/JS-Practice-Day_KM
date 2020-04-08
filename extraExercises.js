e889a3f7c8ea277dcb225c362276dcc7dc6913b9
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-04-08 16:53:36 -0400

    all the exercises finished 4/8

---
+++ extraExercises.js
@@ -0,0 +1,29 @@
+// NUMBER 1
+// function hi(){
+//     console.log("Hello World!");
+// }
+// hi();
+
+// NUMBER 2
+// function greetings(){
+//     var name = prompt("What is your name?");
+//     console.log("Hello " + name + " ! Nice to meet you!");
+// }
+// greetings();
+
+// NUMBER 3
+// function greetings(){
+//     var name = prompt("What is your name?");
+//     if(name==="Bob" || name==="Alice"){
+//         console.log("Hello " + name + " ! Nice to meet you!");
+//     }else{
+//         console.log("You are not Bob or Alice!");
+//     }
+// }
+// greetings();
+
+// NUMBER 4
+function sumOf(){
+    var num = prompt("Give me a number.")
+    
+sumOf();
