60a901862b9443205c81f421e2e9e7c4cb9d65c8
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-04-09 16:20:11 -0400

    completed some more extra exercises

--- extraExercises.js
+++ extraExercises.js
@@ -23,7 +23,68 @@
 // greetings();
 
 // NUMBER 4
-function sumOf(){
-    var num = prompt("Give me a number.")
+// function sumOf(){
+//     var num = prompt("Give me a number.")
+//     var sum = 0;
+//     for(var i=1; i<=num; i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+// console.log(sumOf());
+
+// NUMBER 5
+// function sumOf(){
+//     var num = prompt("Give me a number.")
+//     var sum = 0;
+//     for(var i=1; i<=num; i++){
+//         if(i%3===0 || i%5===0){
+//             sum += i;
+//         }    
+//     }
+//     return sum;
+// }
+// console.log(sumOf());
+
+// NUMBER 6 !!!!
+// function oneOfTwo(){
+//     var num = prompt("Give me a number.");
+//     var total = 0;
+//     var sumOrProduct = prompt("Do you want the sum or product of numbers from 1 to " + num + "? (Please only enter 'sum' or 'Sum' or 'product' or 'Product')");
+//     for(var i=1; i<=num; i++){
+//         if(sumOrProduct === "sum" || sumOrProduct === "Sum"){
+//             total += i;
+//         }else if(sumOrProduct === "product" || sumOrProduct === "Product"){
+//             total *= i;
+//         }
+//     }
+//     return total;
+// }
+// console.log(oneOfTwo());
+
+// NUMBER 7
+// function multiplications(num){
+//     var mult = 0;
+//     for(var i=0; i<=12; i++){
+//         mult = i * num;
+//         console.log(mult);
+//     }
+//     return mult
+// }
+// multiplications(4);
+
+// NUMBER 8
+// function prime(){
     
-sumOf();
+// }
+
+// NUMBER 9
+function guessGame(){
+    var secretNumber = 30;
+    var firstGuess = prompt("Can you guess the secret number between 0-100?");
+    if(firstGuess>secretNumber){
+        
+    }else if(firstGuess<secretNumber){
+
+    }else if(firstGuess)
+}
